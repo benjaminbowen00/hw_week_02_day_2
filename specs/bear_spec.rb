9618ff9749feba9a60c43ce7a50e3c8a42f3e31b
benjaminbowen00 <benjibowen@hotmail.com>
2017-11-07 20:35:48 +0000

    Extra checks on bear catch method

--- specs/bear_spec.rb
+++ specs/bear_spec.rb
@@ -10,7 +10,8 @@ class BearTest < MiniTest::Test
     @bear1 = Bear.new("Barney")
     @fish1 = Fish.new("Mack")
     @fish2 = Fish.new("Sally")
-    @river1 = River.new("Amazon", [@fish1, @fish2])
+    @fish3 = Fish.new("Sol")
+    @river1 = River.new("Amazon", [@fish1, @fish2, @fish3])
   end
 
 
@@ -23,10 +24,10 @@ class BearTest < MiniTest::Test
   end
 
   def test_catch
-    @bear1.catch(@fish1, @river1)
-    assert_equal([@fish1], @bear1.stomach_contents)
-    assert_equal([@fish2], @river1.contents)
-    assert_equal(1, @river1.fish_count())
+    @bear1.catch(@fish2, @river1)
+    assert_equal([@fish2], @bear1.stomach_contents)
+    assert_equal([@fish1, @fish3], @river1.contents)
+    assert_equal(2, @river1.fish_count())
     assert_equal(1, @bear1.food_count())
   end
 
